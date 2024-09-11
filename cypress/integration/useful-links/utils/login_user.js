972b6d720e0e55cd8a176594e58406cbe5c2a493
Serednii Mykola <seredniimykola@gmail.com>
2024-09-11 16:55:30 +0200

    add ci/cd

--- cypress/integration/useful-links/utils/login_user.js
+++ cypress/integration/useful-links/utils/login_user.js
@@ -4,11 +4,18 @@ function verifyLoginUser(email, password) {
     cy.get('.login-form input[type="password"]').type(password);
     cy.contains('Login').click({ force: true });
     cy.get('.user-panel').click({ force: true });
-
+    cy.contains('userullinks').then((el) => {
+        console.log(el)
+    })
     // cy.contains(email, { timeout: 3000 }).parent('tr').within(() => {
     //     cy.get('td').eq(1).find('button').click();
     // });
-    cy.get("input").contains("useful", { timeout: 3000 }).should('exist')
+    //cy.get("input").contains("useful", { timeout: 3000 }).should('exist')
+    // cy.get("input").then(($div) => {
+    //     $div.each((_, el) => {
+    //         console.log(el)
+    //     })
+    // })
     // // Знаходимо td з email, потім знаходимо батьківський tr
     // const tr = $(`td:contains(${email})`).closest('tr');
 
